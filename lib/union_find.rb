d605582d142466c9c01279997420da9351aa9f7f
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2023-11-23 17:51:45 +0900

    format syntax.

--- lib/union_find.rb
+++ lib/union_find.rb
@@ -1,20 +1,41 @@
 class UnionFind
   def initialize(n)
     @g = Array.new(n, -1)
+    @components = n
+  end
+  def add
+    @g << -1
+    @components += 1
+    @g.size - 1
   end
   def leader(i)
-    @g[i] >= 0 ? @g[i] = leader(@g[i]) : i
+    r = j = i
+    r = j until (j = @g[r]) < 0
+    @g[i] = r and i = j until (j = @g[i]) < 0
+    r
   end
-  def merge(a,b)
-    x, y = leader(a), leader(b)
-    return if x == y
+  def same?(i, j)
+    leader(i) == leader(j)
+  end
+  def merge(i,j)
+    x, y = leader(i), leader(j)
+    return false if x == y
     x, y = y, x if -@g[x] < -@g[y]
     @g[x] += @g[y]
     @g[y] = x
+    @components -= 1
   end
-  def count(i); -@g[leader(i)]; end
-  def size; @g.size; end
-  def same?(a,b); leader(a) == leader(b); end
-  def groups; size.times.group_by{ leader(_1) }.values; end
-  def leaders; size.times.select{ @g[_1] < 0}; end
+  def count(i)
+    -@g[leader(i)]
+  end
+  def groups
+    size.times.group_by{|i| leader(i) }.values
+  end
+  def size
+    @g.size
+  end
+  attr_reader :components
+  def connect?
+    @components == 1
+  end
 end
