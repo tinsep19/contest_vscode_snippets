cb32d849205af24fd19edc66ed676edc58c637a8
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2024-01-23 21:34:08 +0900

    return array but not lambda.

--- lib/cumsum.rb
+++ lib/cumsum.rb
@@ -1,9 +1,5 @@
-# usage.
-# s = cumsum([1, 2, 3, 4])
-# p s[0, 4] # => 10 
-# p s[1, 4] # => 5
 def cumsum(array)
   sum = Array.new(array.size + 1, 0)
-  array.each_with_index{|v,i| sum[i + 1] = sum[i] + v }
-  ->(l, r){ sum[r] - sum[l] }
+  array.each_with_index{|x,i| sum[i + 1] = sum[i] + x }
+  sum
 end
