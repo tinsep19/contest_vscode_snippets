9f56cad6da33db2a14dc98ad3112873c6d830a77
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2024-06-05 11:49:26 +0900

    Update cumsum.rb

--- lib/cumsum.rb
+++ lib/cumsum.rb
@@ -1,7 +1,7 @@
 def cumsum(array)
   sum = Array.new(array.size + 1, 0)
   i = 0; n = array.size
-  sum[i] = sum[i - 1] + array[i] while (i += 1) < n
+  sum[i] = sum[i - 1] + array[i - 1] while (i += 1) <= n
   sum
 end
 def rcumsum(array)
