2ab23460bbf8a8ea5f4933b320a52379a84b7118
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2024-06-05 16:51:31 +0900

    Update minmax.rb

--- lib/minmax.rb
+++ lib/minmax.rb
@@ -1,3 +1,7 @@
+class Array
+  def chmin(i, x); self[i] = x if x < self[i]; end
+  def chmax(i, x); self[i] = x if x > self[i]; end
+end
 def _min(a,b); a < b ? a : b; end
 def _max(a,b); a > b ? a : b; end
 
