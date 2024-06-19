68acbe6964dc310d142d21364c7cedc8b6a7711f
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2024-06-19 21:58:39 +0900

    reduce multi assign

--- lib/fibsearch.rb
+++ lib/fibsearch.rb
@@ -2,36 +2,38 @@
 # search min{ f[x] | x \in [l, r) } 
 # if you want max, fix xa > xb -> xa < xb 
 # call fibsearch(l, r){|x| -f(x) }
-
+# a, b, c (a + b)
+# a = b - a
+# b = b - a
 def fibsearch(l, r, &f)
-  offset, a, b = l - 1, 1, 1
-  a, b = b, b + a while offset + b < r
-  xa, xb = nil, nil
+  offset = l - 1; a = b = 1
+  (b += a; a = b - a) while offset + b < r
+  xa = xb = nil
   while b > 1
     xa ||= f[offset + a]
     xb ||= f[offset + b] if offset + b < r
-    a, b = b - a, a
+    a = b - a; b = b - a
     if xb && xa > xb
-      offset, xa, xb = offset + b, xb, nil
+      offset += b; xa = xb; xb = nil
     else
-      xa, xb = nil, xa
+      xb = xa; xa = nil
     end
   end
   return xa || xb
 end
 
 def fibsearch_index(l, r, &f)
-  offset, a, b = l - 1, 1, 1
-  a, b = b, b + a while offset + b < r
+  offset = l - 1; a = b = 1
+  (b += a; a = b - a) while offset + b < r
   xa, xb = nil, nil
   while b > 1
     xa ||= f[offset + a]
     xb ||= f[offset + b] if offset + b < r
-    a, b = b - a, a
+    a = b - a; b = b - a
     if xb && xa > xb
-      offset, xa, xb = offset + b, xb, nil
+      offset += b; xa = xb; xb = nil
     else
-      xa, xb = nil, xa
+      xb = xa; xa = nil
     end
   end
   return offset + 1
