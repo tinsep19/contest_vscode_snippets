4847868225d43e4d4bb620dcd89f78a2eae78507
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2024-10-13 17:32:41 +0900

    create WarshallFroyd class. support update edge.

--- lib/warshall_floyd.rb
+++ lib/warshall_floyd.rb
@@ -1,18 +1,36 @@
 require 'numo/narray'
-INF = 1 << 30
-N, M = gets.split.map(&:to_i)
-g = Numo::Int64.new(N, N).fill(INF)
-g.diagonal.store(0)
-M.times do
-  a,b,c = gets.split.map(&:to_i)
-  a -= 1; b -= 1
-  st[a, b] = c
-end
-sum = 0
+class WarshallFroyd
+  INF = 1 << 60
+  def initialize(n)
+    @g = Numo::Int64.new(n, n).fill(INF)
+    @g.diagonal.store(0)
+    @size = n
+  end
+  def [](a,b)
+    @g[a, b]
+  end
+  def add_edge(a, b, w)
+    @g[a, b] = w
+    @g[b, a] = w
+  end
+  def optimize!
+    @size.times do |k|  
+      ik = @g[true, k].expand_dims(0).transpose
+      kj = @g[k, true]
+      @g = Numo::Int64.minimum(@g, ik + kj)
+    end
+  end
+  def update!(a, b, w)
+    return if @g[a,b] <= w
+    @g[a, b] = w
+    @g[b, a] = w
 
-N.times do |k|  
-  ik = g[true, k].expand_dims(0).transpose
-  kj = g[k, true]
-  g = Numo::Int64.minimum(g, ik + kj)
+    ia = @g[true, a].expand_dims(0).transpose
+    bj = @g[b, true]
+    @g = Numo::Int64.minimum(@g, ia + bj + w)
+
+    ib = @g[true, b].expand_dims(0).transpose
+    aj = @g[a, true]
+    @g = Numo::Int64.minimum(@g, ib + aj + w)
+  end
 end
-puts sum
