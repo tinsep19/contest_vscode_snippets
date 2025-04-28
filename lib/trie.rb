7a976292b254f12fe9d89789812452aca2c4e422
TANIGUCHI Kousuke <kousuke.taniguchi@gmail.com>
2025-04-28 21:04:42 +0900

    refactor it after ABC403

--- lib/trie.rb
+++ lib/trie.rb
@@ -1,28 +1,74 @@
 class Trie
   def initialize
-    @node = []; @count = []; 
+    @node   = []
+    @leaf   = []
+    @prefix = []
     @root = add_node
   end
+  def size; prefix[@root]; end
 
-  def size; count[@root]; end
-
-  # add(seq, n)
-  def add(seq, n = 1)
-    @count[@root] += n
-    seq.inject(@root){|u, label| node = @node[u]; node[label] ||= add_node(n) }
+  def insert(seq, n = 1)
+    t = seq.inject(@root) do |u, c|
+      @prefix[u] += n
+      node[u][c] ||= add_node
+    end
+    @prefix[t] += n
+    @leaf[t]   += n
+    return t
   end
   
-  # follow(seq){|label, count| ... }
-  def follow(seq, &block)
-    return enum_for(:follow, seq) if !block
-    seq.inject(@root){|u, label| v = @node[u][label]; break unless v; block[label, @count[v]] ; v}
+  # delete leaf which represented by seq
+  def delete(seq, n = 1)
+    t = node_id(seq)
+    return if !t || @leaf[t] < n
+    seq.inject(@root) do |u, c|
+      @prefix[u] -= n
+      node[u][c]
+    end
+    @prefix[t] -= n
+    @leaf[t]   -= n
+  end
+  
+  # disjoint subtree by node which represented by preseq 
+  def disjoint(preseq, n)
+    t = node_id(preseq)
+    return if !t
+    f = @prefix[t]
+    preseq.inject(@root) do |u, c|
+      @prefix[u] -= f
+      @node[u][c]
+    end
+    @prefix[t] = 0
+    @leaf[t]   = 0
+    @node[t]   = {}
+    t
+  end
+
+  # return count of leaf which represented by seq
+  def leaf(seq)
+    t = node_id(seq)
+    t && @leaf[t]
+  end
+  
+  # return count of leaf which has seq as prefix
+  def prefix(seq)
+    t = node_id(seq)
+    t && @prefix[t]
   end
   
   private
+  def node_id(seq)
+    seq.inject(@root) do |u, c|
+      node = @node[u]
+      return if !node[c]
+      node[c]
+    end
+  end
   def add_node(n = 0)
     id = @node.size
-    @node << {}
-    @count << n
+    @leaf   << 0
+    @prefix << 0
+    @node   << {}
     id
   end
 end
